caf45cab04f5d4e10ac4744ef9de1f19b12594e3
git <jinzhanjun@pb-station.com>
2018-06-25 17:13:33 +0800

    Merge branch 'master' of 172.16.33.4:/home/gitrepo/gittest
    # Conflicts:
    #	case/PC_ruixin/IdentityInformation/editinfo.py

--- case/PC_ruixin/scenario/test_scenario.py
+++ case/PC_ruixin/scenario/test_scenario.py
@@ -70,7 +70,7 @@ class TestScenario(unittest.TestCase):
 
 
     def test_purchase_through(self):
-        '''申购-审核-通过'''
+        '''申购-审核-通过-合同管理编辑-确认付款-回访单确认-想买'''
         try:
             self.browser.open_url('http://inv.pb-yun.com')
             sleep(1)
@@ -92,12 +92,87 @@ class TestScenario(unittest.TestCase):
             sleep(1)
             self.assertEqual(self.browser.manage_status(),'已通过')                #进入列表页面进行断言
             sleep(1)
-
+            self.browser.contract_operation(self.assertEqual,'未付款','未回访')    #进入合同管理列表页面，数据状态断言，点击操作按钮
+            sleep(1)
+            self.browser.conract_edit_button('2018-04-26',1,'刘鑫','0')           #输入时间、手续费、经办人、冷静期保存
+            sleep(1)
+            self.browser.conract_payment()                                          #确认已付款页面点击进行确认
+            sleep(1)
+            self.assertEqual(self.browser.conract_status(),'已付款')               #数据状态断言-已付款
+            self.browser.from_returm()                                              #回访单页面进行确认按钮
+            sleep(1)
+            self.assertEqual(self.browser.return_status(),'已发送')                #数据状态断言-已发送
+            sleep(1)
+            self.browser.open_url('http://inv.pb-yun.com')                        #登录投资者PC端
+            sleep(1)
+            self.browser.even_submit()                                              #PC回访单点击是提交
+            sleep(1)
+            self.browser.open_url('http://boss.pb-yun.com/')                      # 进入管理端
+            sleep(1)
+            self.browser.ManageLogin('13511055879', '123456', '')                 # 管理端输入账号密码
+            sleep(1)
+            WebDriverWait(self.driver, 'By.XPATh', '//*[@id="detailName"]/span') # 显示等待
+            sleep(1)
+            self.browser.sell_contract()                                            #点击销售管理-合同管理菜单
+            sleep(1)
+            self.assertEqual(self.browser.return_status(),'想买')                  #列表数据状态断言-想买
 
 
         except Exception as msg:
             self.log.info(msg)
             raise
+
+    def test_purchase_visitrejected(self):
+        '''申购-审核-通过-合同管理编辑-确认付款-回访单确认-想买'''
+        try:
+            self.browser.open_url('http://inv.pb-yun.com')
+            sleep(1)
+            self.browser.pc_login('15822816936', 'abc123456', '1')                # 登录
+            sleep(1)
+            self.browser.purodut_scenarion(self.assertEqual, 2000)                  # PC申购--提交成功
+            sleep(1)
+            self.browser.purchase_record(self.assertEqual,'未审核')                 # 进入申赎记录页面获取数据状态断言
+            sleep(1)
+            self.browser.open_url('http://boss.pb-yun.com/')                      # 进入管理端
+            sleep(1)
+            self.browser.ManageLogin('13511055879', '123456', '')                 # 管理端输入账号密码
+            sleep(3)
+            WebDriverWait(self.driver, 'By.XPATh', '//*[@id="detailName"]/span') # 显示等待
+            sleep(1)
+            self.browser.appointment_apply(self.assertEqual)                        # 进入销售管理-预约申请页面-获取数据状态-断言
+            sleep(1)
+            self.browser.determine()                                                #审核通过
+            sleep(1)
+            self.assertEqual(self.browser.manage_status(),'已通过')                #进入列表页面进行断言
+            sleep(1)
+            self.browser.contract_operation(self.assertEqual,'未付款','未回访')    #进入合同管理列表页面，数据状态断言，点击操作按钮
+            sleep(1)
+            self.browser.conract_edit_button('2018-04-26',1,'刘鑫','0')           #输入时间、手续费、经办人、冷静期保存
+            sleep(1)
+            self.browser.conract_payment()                                          #确认已付款页面点击进行确认
+            sleep(1)
+            self.browser.from_returm()                                              #回访单页面进行确认按钮
+            sleep(1)
+            self.assertEqual(self.browser.conract_status(),'已付款')               #数据状态断言-已付款
+            sleep(1)
+            self.browser.open_url('http://inv.pb-yun.com')                        #登录投资者PC端
+            sleep(1)
+            self.browser.odd_number()                                               #PC回访单点击否提交
+            sleep(1)
+            self.browser.open_url('http://boss.pb-yun.com/')                      # 进入管理端
+            sleep(1)
+            self.browser.ManageLogin('13511055879', '123456', '')                  # 管理端输入账号密码
+            sleep(1)
+            WebDriverWait(self.driver, 'By.XPATh', '//*[@id="detailName"]/span')  # 显示等待
+            sleep(1)
+            self.browser.sell_contract()                                             # 点击销售管理-合同管理菜单
+            sleep(1)
+            self.assertEqual(self.browser.return_status(), '不想买')                 # 列表数据状态断言-想买
+
+        except Exception as msg:
+            self.log.info(msg)
+            raise
+
 
 
 if __name__ == "__main__":
