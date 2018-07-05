6545203107acf3ec501664785aa3b3b27367a408
wangyunfei <wangyunfei@pb-station.com>
2018-07-05 14:18:29 +0800

    fei

---
+++ common/redis_client.py
@@ -0,0 +1,24 @@
+"""
+@author:fei
+@date:2018-7-5
+@brief:链接Redis数据库，获取验证码
+"""
+
+import redis
+
+
+class RedisClient(object):
+    """获取验证码"""
+
+    pool = redis.ConnectionPool(host='172.16.33.4', password='REDIS123', port=6379, db=2, decode_responses=True)
+    r = redis.Redis(connection_pool=pool)
+
+    def get_code(self, phone):
+        code = self.r.get(phone)
+        return code
+
+
+if __name__ == '__main__':
+    r = RedisClient()
+    b = r.get_code('17600661017')
+    print(b)
